8624741aa265357c0ca790117ee6c7d4eebccb1d
theProf <theProf@gerggle.com>
2013-11-05 11:59:02 -0500

    added views

---
+++ bob-smibert-deals/views/monthly.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Represents the view for the public-facing component of the plugin.
+ *
+ * This typically includes any information, if any, that is rendered to the
+ * frontend of the theme when the plugin is activated.
+ *
+ * @package   Bob_Smibert_Deals
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2013 Your Name or Company Name
+ */
+?>
+
+<!-- This file is used to markup the public facing aspect of the plugin. -->
+
+<article id="monthly-deals" class="">
+	<h2>Bobs November Super Sale</h2>
+	<ul>
+		<li>
+			<strong>10 Sessions</strong>
+			<p>@ $10.50/session</p>
+			<p></p>
+		</li>
+		<li>
+			<strong>20 Sessions</strong>
+			<p>@ $8.50/session</p>
+			<p></p>
+		</li>
+		<li>
+			<strong>40 Sessions</strong>
+			<p>@ $7.50/session</p>
+			<p></p>
+		</li>
+	</ul>
+</article>
+
+[wp_cart:Session Deals:price:[Options|10 Sessions @ $10.50/session,118.65|20 Sessions @ $8.50/session,192.10|40 Sessions @ $7.50/session, 339.00]:end]
+
+<p><span>note: all session packages have a 1 year expiry date once purchased</span></p>
